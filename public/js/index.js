9f4be320215ea29adae057d769bef94b1fe366b4
1032569390hj <1032569390@qq.com>
2020-11-08 15:57:06 +0800

    updata

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 import './library/jquery.js';
 import './library/swiper-3.4.2.jquery.min.js';
 
+let str ="qqqq";
 var mySwiper = new Swiper('.swiper-container', {
   autoplay: 3000,//可选选项，自动滑动
   effect : 'fade',
@@ -13,3 +14,5 @@ var mySwiper = new Swiper('.swiper-container', {
     crossFade: true,
   }
 })
+
+let num = 111;

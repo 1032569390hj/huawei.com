a4ca9af995fb34637493e8eff03555f7f5d83ad2
1032569390hj <1032569390@qq.com>
2020-11-10 22:45:19 +0800

    首页

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,59 @@
 import './library/jquery.js';
 import './library/swiper-3.4.2.jquery.min.js';
+import './library/jquery.lazyload.min.js';
 
-var mySwiper = new Swiper('.swiper-container', {
+//第一个轮播图
+let mySwiper = new Swiper('.swiper-container', {
   autoplay: 3000,//可选选项，自动滑动
-  effect : 'fade',
-  loop : true,
-  pagination: '.swiper-pagination',
-  paginationClickable: true,
-  prevButton:'.swiper-button-prev',
-  nextButton:'.swiper-button-next',
+  effect : 'fade', //切换效果
+  loop : true,     //循环播放
+  pagination: '.swiper-pagination',  //分页器
+  paginationClickable: true,  //点击分页器切换图片
+  prevButton:'.swiper-button-prev',  //上一张按钮
+  nextButton:'.swiper-button-next',  //下一张按钮
+  fade: {
+    crossFade: true,
+  }
+})
+//鼠标悬停时停止轮播
+let comtainer = $('.swiper-container');
+comtainer.on('mouseenter', function() {
+  mySwiper.stopAutoplay();
+});
+//鼠标离开后继续轮播
+comtainer.on('mouseleave', function() {
+  mySwiper.startAutoplay();
+});
+
+//广告轮播图
+let bannerSwiper = new Swiper('#bannerswiper', {
+  autoplay: 3000,//可选选项，自动滑动
+  effect : 'fade', //切换效果
+  loop : true,     //循环播放
+  pagination: '.swiper-pagination',  //分页器
+  paginationClickable: true,  //点击分页器切换图片
+  prevButton:'.swiper-button-prev',  //上一张按钮
+  nextButton:'.swiper-button-next',  //下一张按钮
   fade: {
     crossFade: true,
   }
 })
 
-var comtainer = $('.swiper-container');
-comtainer.on('mouseenter', function() {
-  mySwiper.stopAutoplay();
-});
-comtainer.on('mouseleave', function() {
-  mySwiper.startAutoplay();
-});
-
-var mySwiper = new Swiper('.swipers', {
+//商品轮播
+let listSwiper = new Swiper('.swipers', {
   prevButton:'.swiper-button-prev',
   nextButton:'.swiper-button-next',
-})
+});
+
+var fdline = new Swiper('.fd-line',{
+  prevButton:'.swiper-button-prev',
+  nextButton:'.swiper-button-next',
+  })
+
+//图片懒加载
+$(function() {
+  $("img.lazy").lazyload({
+    placeholder: "../img/layzeimg.gif",
+    effect: "fadeIn",
+  });
+});
